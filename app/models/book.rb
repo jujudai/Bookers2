e2223ced55c0f21378806259bd589cdfe5cbe30f
jujudai <takasawad0506gmail.com>
2024-12-02 23:23:18 +0000

    [start]Bootstrap導入

--- app/models/book.rb
+++ app/models/book.rb
@@ -1,3 +1,4 @@
 class Book < ApplicationRecord
   has_one_attached :image1
+  belongs_to :user
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,16 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+  has_many :books, dependent: :destroy
+
+  has_one_attached :book
+
+  
+  def get_book
+    unless book.attached?
+      file_path = Rails.root.join('app/assets/images/sample-author1.jpg')
+      book.attach(io: File.open(file_path), filename: 'default-image.jpg', content_type: 'image/jpeg')
+    end
+    book.variant(resize_to_limit: [100, 100]).processed
+  end
 end
