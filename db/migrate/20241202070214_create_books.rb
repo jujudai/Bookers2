ce431d47c8968a267db02f630870583f3b0f71e6
jujudai <takasawad0506gmail.com>
2024-12-03 00:04:37 +0000

    [finish]ファイルの設定

--- db/migrate/20241202070214_create_books.rb
+++ db/migrate/20241202070214_create_books.rb
@@ -2,8 +2,10 @@ class CreateBooks < ActiveRecord::Migration[6.1]
   def change
     create_table :books do |t|
 
-      t.string :title
-      t.string :body
+      t.string :Title
+      t.string :Opinion
+      t.integer :user_id
+      
       t.timestamps
     end
   end
