2852e7e8b2c0cc896dbda35a12274a903ff72ffe
jujudai <takasawad0506gmail.com>
2024-12-13 15:05:08 +0000

    [start]userのindexページの作成

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,10 +1,15 @@
 class UsersController < ApplicationController
   def show
+    @book = Book.find(params[:id])
+    @user = @book.user
   end
 
   def edit
   end
 
   def index
+    @user = current_user
+    @book = Book.new
+    @books = Book.all
   end
 end
