f61bc35429aa9d85ab28084c18f98103d963d243
jujudai <takasawad0506gmail.com>
2024-12-15 08:00:40 +0000

    [start]userのshowページの作成

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,15 +1,33 @@
 class UsersController < ApplicationController
   def show
     @book = Book.find(params[:id])
+    @user = User.find(params[:id])
     @user = @book.user
   end
 
   def edit
+    @user = User.find(params[:id])
+  end
+
+  def update
+    @user = User.find(params[:id])
+    if @user.update(user_params)
+      redirect_to @user, notice: 
+    else
+      render :edit, status: :unprocessable_entity
+    end
   end
 
   def index
+    @users = User.all
     @user = current_user
     @book = Book.new
     @books = Book.all
+  end
+
+  private
+
+  def user_params
+    params.require(:user).permit(:name, :profile_image, :introduction)
   end
 end
